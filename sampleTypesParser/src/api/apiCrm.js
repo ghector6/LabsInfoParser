d7793cc03cdca5ae534d4c139e6c62c588f7bd74
Hector Garcia Rosales <ghectormus@gmail.com>
2024-02-14 10:23:20 -0600

    solve method parsing

--- sampleTypesParser/src/api/apiCrm.js
+++ sampleTypesParser/src/api/apiCrm.js
@@ -1,7 +1,10 @@
-
+require('dotenv').config();
 const axios = require('axios');
+
 const API = process.env.NEXT_PUBLIC_API_CRM;
 
+
+//console.log(process.env.NEXT_PUBLIC_API_CRM, 'API')
 module.exports = axios.create({
   baseURL: `${API}/api`
 });
