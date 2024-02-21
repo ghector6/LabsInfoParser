73878a1cf5da6bd375d5edb8513f1a653d080aac
Hector Garcia Rosales <ghectormus@gmail.com>
2024-02-21 13:32:09 -0600

    #1 add: parser

---
+++ sampleTypesParser/src/sectionParser.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const createSection = require("./services/SectionService");
+const filePath =
+  "/Users/ghector6/MakingDevsProjects/VitaliaUtils/sampleTypesParser/src/utils/catalogoDeSecciones.csv";
+function parseSection(filePath) {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err, "Error reading file");
+      return;
+    }
+    const test = data.trim().split("\n");
+    test.map(processRow);
+  });
+}
+
+async function processRow(row) {
+  const data = row.split(",");
+  const postRequest = {
+    data: {
+      sectionName: data[0],
+      prefix: "",
+      status: "created",
+    },
+  };
+  try {
+    console.log(postRequest);
+  } catch (error) {
+    console.error(error, "Error creating section");
+  }
+}
+
+parseSection(filePath);
